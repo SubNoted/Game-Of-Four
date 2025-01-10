126685e97144cda8c23ace932aa7b762c11c5922
SubNoted <superprogergod@mail.ru>
2025-01-10 17:30:08 +0300

    add pushImageTriangleToCanvas

--- lib/LowPolySubnoted/render/basicStrategy.cpp
+++ lib/LowPolySubnoted/render/basicStrategy.cpp
@@ -1,114 +1,109 @@
 #include "basicStrategy.h"
 
-///////////////////* my extra (for TFT_eSPI) /////////////
-// Clipping macro for pushImage
-#define PI_CLIP                                        \
-                                                       \
-  int32_t dx = 0;                                      \
-  int32_t dy = 0;                                      \
-  int32_t dw = w;                                      \
-  int32_t dh = h;                                      \
-                                                       \
-  if (dw < 1 || dh < 1) return;
 
-void pushImageTriangleToSprite(int32_t x1,int32_t y1, int32_t x2,int32_t y2, int32_t x3,int32_t y3, uint32_t color)
+#include "cloudsTex.h"
+
+///////////////////* my extra (for TFT_eSPI (ripped of TFT_eSPI) ) /////////////
+
+void  pushImage(int32_t x, int32_t y, int32_t w, int32_t h, uint16_t *_img, uint8_t *data)
 {
-
-}
-
-void  pushImage(int32_t x, int32_t y, int32_t w, int32_t h, uint16_t *_img, uint16_t *data)
-{
-    if (data == nullptr) return;
-
-    PI_CLIP;    
+    if (data == nullptr) return;  
 
     // Pointer within original image
-    uint8_t *ptro = (uint8_t *)data + ((dx + dy * w) << 1);
+    uint8_t *ptro = data + ((x + y * 256) << 1);
     // Pointer within sprite image
     uint8_t *ptrs = (uint8_t *)_img + ((x + y * SCRN_WIDTH) << 1);
 
-    while (dh--)
+    while (h--)
     {
-    memcpy(ptrs, ptro, dw<<1);
-    ptro += w << 1;
-    ptrs += SCRN_WIDTH << 1;
+        memcpy(ptrs, ptro, (w<<1));
+        ptro += 256<<1;
+        ptrs += SCRN_WIDTH<<1;
     }
 }
 
-// void fillTriangle ( int32_t x0, int32_t y0, int32_t x1, int32_t y1, int32_t x2, int32_t y2, uint32_t color)
-// {
-//   int32_t a, b, y, last;
+void pushImageTriangleToSprite(int32_t x0, int32_t y0, int32_t x1, int32_t y1, int32_t x2, int32_t y2, uint16_t* _img, uint8_t* data)
+{
+    int32_t a, b, y, last;
+    
 
-//   // Sort coordinates by Y order (y2 >= y1 >= y0)
-//   if (y0 > y1) {
-//     transpose(y0, y1); transpose(x0, x1);
-//   }
-//   if (y1 > y2) {
-//     transpose(y2, y1); transpose(x2, x1);
-//   }
-//   if (y0 > y1) {
-//     transpose(y0, y1); transpose(x0, x1);
-//   }
+    // Sort coordinates by Y order (y2 >= y1 >= y0)
+    if (y0 > y1) {
+        transpose(y0, y1); transpose(x0, x1);
+    }
+    if (y1 > y2) {
+        transpose(y2, y1); transpose(x2, x1);
+    }
+    if (y0 > y1) {
+        transpose(y0, y1); transpose(x0, x1);
+    }
 
-//   if (y0 == y2) { // Handle awkward all-on-same-line case as its own thing
-//     a = b = x0;
-//     if (x1 < a)      a = x1;
-//     else if (x1 > b) b = x1;
-//     if (x2 < a)      a = x2;
-//     else if (x2 > b) b = x2;
-//     drawFastHLine(a, y0, b - a + 1, color);
-//     return;
-//   }
+    if (y0 == y2) { // Handle awkward all-on-same-line case as its own thing
+        a = b = x0;
+        if (x1 < a)      a = x1;
+        else if (x1 > b) b = x1;
+        if (x2 < a)      a = x2;
+        else if (x2 > b) b = x2;
 
-//   //begin_tft_write();          // Sprite class can use this function, avoiding begin_tft_write()
-//   inTransaction = true;
+        // drawFastHLine(a, y0, b - a + 1, color);
+        // sprite.drawFastHLine(a, y0, b - a + 1, TFT_DARKGREY);
 
-//   int32_t
-//   dx01 = x1 - x0,
-//   dy01 = y1 - y0,
-//   dx02 = x2 - x0,
-//   dy02 = y2 - y0,
-//   dx12 = x2 - x1,
-//   dy12 = y2 - y1,
-//   sa   = 0,
-//   sb   = 0;
+        pushImage(a, y0, b - a + 1, 1, _img, data);
 
-//   // For upper part of triangle, find scanline crossings for segments
-//   // 0-1 and 0-2.  If y1=y2 (flat-bottomed triangle), the scanline y1
-//   // is included here (and second loop will be skipped, avoiding a /0
-//   // error there), otherwise scanline y1 is skipped here and handled
-//   // in the second loop...which also avoids a /0 error here if y0=y1
-//   // (flat-topped triangle).
-//   if (y1 == y2) last = y1;  // Include y1 scanline
-//   else         last = y1 - 1; // Skip it
 
-//   for (y = y0; y <= last; y++) {
-//     a   = x0 + sa / dy01;
-//     b   = x0 + sb / dy02;
-//     sa += dx01;
-//     sb += dx02;
+        return;
+    }
 
-//     if (a > b) transpose(a, b);
-//     drawFastHLine(a, y, b - a + 1, color);
-//   }
+    int32_t
+    dx01 = x1 - x0,
+    dy01 = y1 - y0,
+    dx02 = x2 - x0,
+    dy02 = y2 - y0,
+    dx12 = x2 - x1,
+    dy12 = y2 - y1,
+    sa   = 0,
+    sb   = 0;
 
-//   // For lower part of triangle, find scanline crossings for segments
-//   // 0-2 and 1-2.  This loop is skipped if y1=y2.
-//   sa = dx12 * (y - y1);
-//   sb = dx02 * (y - y0);
-//   for (; y <= y2; y++) {
-//     a   = x1 + sa / dy12;
-//     b   = x0 + sb / dy02;
-//     sa += dx12;
-//     sb += dx02;
+    // For upper part of triangle, find scanline crossings for segments
+    // 0-1 and 0-2.  If y1=y2 (flat-bottomed triangle), the scanline y1
+    // is included here (and second loop will be skipped, avoiding a /0
+    // error there), otherwise scanline y1 is skipped here and handled
+    // in the second loop...which also avoids a /0 error here if y0=y1
+    // (flat-topped triangle).
+    if (y1 == y2) last = y1;  // Include y1 scanline
+    else         last = y1 - 1; // Skip it
 
-//     if (a > b) transpose(a, b);
-//     drawFastHLine(a, y, b - a + 1, color);
-//   }
+    for (y = y0; y <= last; y++) {
+        a   = x0 + sa / dy01;
+        b   = x0 + sb / dy02;
+        sa += dx01;
+        sb += dx02;
 
-//   inTransaction = lockTransaction;
-//   end_tft_write();              // Does nothing if Sprite class uses this function
-// }
+        if (a > b) transpose(a, b);        
+        // sprite.drawFastHLine(a, y, b - a + 1, TFT_DARKGREY);
+        
+        pushImage(a, y, b - a + 1, 1, _img, data);
+        
+    }
+
+    // For lower part of triangle, find scanline crossings for segments
+    // 0-2 and 1-2.  This loop is skipped if y1=y2.
+    sa = dx12 * (y - y1);
+    sb = dx02 * (y - y0);
+    for (; y <= y2; y++) {
+        a   = x1 + sa / dy12;
+        b   = x0 + sb / dy02;
+        sa += dx12;
+        sb += dx02;
+
+        if (a > b) transpose(a, b);
+        // sprite.drawFastHLine(a, y, b - a + 1, TFT_DARKGREY);
+
+        pushImage(a, y, b - a + 1, 1, _img, data);
+
+    }
+}
+
 
 
 void BasicRendererStrategy::renderScene(std::shared_ptr<Scene> scene, TFT_eSPI& tft, TFT_eSprite* canvas, uint16_t** cnvsPtr) 
@@ -136,13 +131,19 @@ void BasicRendererStrategy::renderScene(std::shared_ptr<Scene> scene, TFT_eSPI& 
             color = canvas[cnvsNum].alphaBlend(shade*255, color, TFT_CYAN);
             
             // canvas[cnvsNum].pushImage
-            // canvas[cnvsNum].fillTriangle 
+            // canvas[cnvsNum].fillTriangle
 
-            canvas[cnvsNum].fillTriangle(
+            // pushImage(100,100, 100, 100, cnvsPtr[cnvsNum], cloudsTex);
+
+            // canvas[cnvsNum].pushImage(0,0, 256, 256, cloudsTex);
+
+            pushImageTriangleToSprite(
                 verts[0].x, verts[0].y,
                 verts[1].x, verts[1].y,
                 verts[2].x, verts[2].y,
-                color
+                cnvsPtr[cnvsNum],
+                cloudsTex
+                // color
             );
 #if DEBUG_MODE
             canvas[cnvsNum].drawTriangle(
