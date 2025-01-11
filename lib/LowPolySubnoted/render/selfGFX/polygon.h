0d04ab1d2c5729363139ba63c41056b5c8df8cd5
SubNoted <superprogergod@mail.ru>
2025-01-11 03:40:31 +0300

    add polygon without texture

--- lib/LowPolySubnoted/render/selfGFX/polygon.h
+++ lib/LowPolySubnoted/render/selfGFX/polygon.h
@@ -5,6 +5,19 @@
 // #include <scene/scene.h>
 #include <vector>
 
+class PolygonWOTex
+{
+private:
+
+public:
+    uint16_t vertices[3]; //number of vertices in scene
+    
+    PolygonWOTex(void);
+    PolygonWOTex(const uint16_t& v0, const uint16_t& v1, const uint16_t& v2);
+    void setVertices(const uint16_t& v0, const uint16_t& v1, const uint16_t& v2);
+    void draw();
+};
+
 class Polygon
 {
 private:
