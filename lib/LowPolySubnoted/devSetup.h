243632121e1535020470838009f3f27a555d1d77
SubNoted <superprogergod@mail.ru>
2025-03-01 02:47:44 +0300

    esp32-s3-zero test

--- lib/LowPolySubnoted/devSetup.h
+++ lib/LowPolySubnoted/devSetup.h
@@ -12,7 +12,7 @@
 
 //////////////////display info/////////////////
 #define SCRN_WIDTH 240
-#define SCRN_HEIGHT 280
+#define SCRN_HEIGHT 240
 
 #define CENTR_X SCRN_WIDTH / 2
 #define CENTR_Y SCRN_HEIGHT / 2
